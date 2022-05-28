1ea106ec31ae24399d3cbbf3cc31da28747715f5
rus <mir.romzes@mail.ru>
2022-05-28 10:38:45 +0300

    Extends and block

--- travel/views.py
+++ travel/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def home(request):
     name = 'bob'
     return render(request, 'home.html', {'name': name})
+
+
+def about(request):
+    name = 'About Us'
+    return render(request, 'about.html', {'name': name})
