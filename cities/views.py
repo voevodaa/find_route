1db8e8f46932d9f952bdb6d43c19d2d57fea400d
rus <mir.romzes@mail.ru>
2022-06-08 10:24:20 +0300

    Использовал формы от Django и методом Post добавил новый город в бд (models) с уникальным именем

--- cities/views.py
+++ cities/views.py
@@ -1,18 +1,24 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
 
+from cities.forms import CityForm
 from cities.models import City
 
 
 def home(request, pk=None):
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            form.save()
     #if pk:
         #city = City.objects.filter(id=pk).first()
         #context = {'object': city}
         #return render(request, 'cities/detail.html', context)
     #else:
-
+    form = CityForm()
     queue_set = City.objects.all()
-    context = {'object_list': queue_set}
+    context = {'object_list': queue_set, 'form': form}
     return render(request, 'cities/home.html', context)
 
 
